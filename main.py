50fd65b77b2d01e39749d689fa776f69641ef6ca
adam-gill <110919227+adam-gill@users.noreply.github.com>
2024-01-17 22:14:09 -0500

    added gn and gm functions

--- main.py
+++ main.py
@@ -13,4 +13,12 @@ bot = telebot.TeleBot(API_KEY)
 def greet(message):
     bot.reply_to(message, "Hey whats up?")
 
+@bot.message_handler(commands=['gn'])
+def gn(message):
+    bot.reply_to(message, "Goodnight 🌙")
+
+@bot.message_handler(commands=['gm'])
+def gm(message):
+    bot.reply_to(message, "Good morning ☀️")
+
 bot.polling()
